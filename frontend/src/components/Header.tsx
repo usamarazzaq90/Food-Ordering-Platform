84cb0a5b6cf93b343292107597b1033f70f46f49
usamarazzaq90 <usamarazzaq9014@gmail.com>
2026-01-28 23:00:47 +0500

    mobile nav and desktop nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-// import MobileNav from "./MobileNav";
-// import MainNav from "./MainNav";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -10,14 +10,14 @@ const Header = () => {
           to="/"
           className="text-3xl font-bold tracking-tight text-orange-500"
         >
-          MernEats.com
+          BonApetite
         </Link>
-        {/* <div className="md:hidden">
+        <div className="md:hidden">
           <MobileNav />
         </div>
         <div className="hidden md:block">
           <MainNav />
-        </div> */}
+        </div>
       </div>
     </div>
   );
