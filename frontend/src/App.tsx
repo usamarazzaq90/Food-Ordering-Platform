c36540f01e41aaf5c630e6cd63adb329b5503ada
usamarazzaq90 <usamarazzaq9014@gmail.com>
2026-01-28 21:44:10 +0500

    Setup the frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="text-amber-100">This is a button!</Button>
+      <h1 className="text-green-950">I'm Usama</h1>
+    </>
+  );
+}
+
+export default App;
